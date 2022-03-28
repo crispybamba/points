7778fe63ba8d4ae43775d9c244441f86f61c5077
student <student@A28-6>
2022-03-28 09:56:49 +0300

    Add project files.

---
+++ points/Point2d.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace points
+{
+    class Point2d
+    {
+        private int x;
+        private int y;
+
+        public Point2d(){}
+        public Point2d(int x, int y) { this.x = x; this.y = y; }
+
+
+        public int GetX() { return x; }
+        public int GetY() { return y; }
+        public void SetX(int x) { this.x = x; }
+        public void SetY(int y) { this.x = y; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("(x,y) = (");
+            sb.Append(x.ToString());
+            sb.Append(",");
+            sb.Append(y.ToString());
+            sb.Append(")");
+            string str = sb.ToString();
+            return str;
+        }
+
+        public double Distance(Point2d secPoint)
+        {
+            double xPart = Math.Pow((x - secPoint.x), 2);
+            double yPart = Math.Pow((y - secPoint.y), 2);
+            double res = Math.Pow(xPart, 2) + Math.Pow(yPart, 2);
+            res = Math.Sqrt(res);
+            return res;
+        }
+            
+
+    }
+}
---
+++ points/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Text;
+
+namespace points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2d pt1 = new Point2d(20, 40);
+            Point2d pt2 = new Point2d(2, 8);
+            double dist = pt1.Distance(pt2);
+        }
+
+        public static Point2d[] Closest(Point2d[] points)
+        {
+            double closestDis = points[0].Distance(points[1]);
+            Point2d[] closest = new Point2d[2];
+            for (int i = 1; i < points.Length -1 ; i++)
+                for (int j = i+1; j < points.Length; j++)
+                    if (points[i].Distance(points[j]) < closestDis)
+                    {
+                        closest[0] = points[i];
+                        closest[1] = points[j];
+                        closestDis = points[i].Distance(points[j]);
+                    }
+            return closest;
+        }
+    }
+}
